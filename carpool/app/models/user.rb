5d2a9e56a5e43bdb0eb1e8b8e0c33cfc0a868040
siwakotisurya <siwakotisurya@outlook.com>
2015-10-15 16:17:09 +0545

    link changed in for share car new in pool_car html file

--- carpool/app/models/user.rb
+++ carpool/app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :share_cars       
+  has_many :share_cars
+    
 end
