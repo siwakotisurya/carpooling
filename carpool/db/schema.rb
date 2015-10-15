a2ab810b1f5ca6abf59a2f5b535080e7c8acd681
siwakotisurya <siwakotisurya@outlook.com>
2015-10-15 22:28:35 +0545

    form set up in new page of share_cars view

--- carpool/db/schema.rb
+++ carpool/db/schema.rb
@@ -13,6 +13,26 @@
 
 ActiveRecord::Schema.define(version: 20151015162948) do
 
+  create_table "owners", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "full_name"
+    t.string   "image"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "owners", ["email"], name: "index_owners_on_email", unique: true
+  add_index "owners", ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+
   create_table "share_cars", force: :cascade do |t|
     t.string   "departure_point"
     t.string   "destination"
